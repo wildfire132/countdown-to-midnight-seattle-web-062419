e15b2db18577371d29743bad8fd30b17d45fa540
Katana Tran <tkatyk21@gmail.com>
2019-06-18 03:33:22 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,13 @@ def countdown(interger)
     end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(interger)
+    counter = interger.to_int
+    while counter != 0
+      puts "#{counter} SECOND(S)!"
+      sleep(1)
+      counter -= 1
+    end
+  return "HAPPY NEW YEAR!"
+end
