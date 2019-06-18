b88ad58f42aa5057f324687bad49b57ded67fb41
Katana Tran <tkatyk21@gmail.com>
2019-06-18 03:35:48 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(interger)
+    counter = interger.to_int
+    while counter != 0
+      puts "#{counter} SECOND(S)!"
+      counter -= 1
+    end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(interger)
+    counter = interger.to_int
+    while counter != 0
+      puts "#{counter} SECOND(S)!"
+      sleep(1)
+      counter -= 1
+    end
+  return "HAPPY NEW YEAR!"
 end
